12f3b80a8c5c4f4abd1b0997d5e83ba697278c99
songtianyi <songtianyi630@163.com>
2020-06-23 18:04:43 +0800

    fix: better solution for slice out of range panic

--- modules/classifiers/mqtt.go
+++ modules/classifiers/mqtt.go
@@ -15,9 +15,14 @@ func (classifier MQTTClassifier) HeuristicClassify(flow *types.Flow) bool {
 	return checkFirstPayload(flow.GetPackets(), layers.LayerTypeTCP,
 		func(payload []byte, packetsRest []gopacket.Packet) bool {
 			//check Control packet (connect)
+			if len(payload) < 6 {
+			    // at least 6 packets
+			    // 0x10 0x04 0x00 0x00 M Q
+                            return false
+                        }
 			isValidPacket := payload[0] == 0x10
 			//check message lenght
-			isValidLenght := int(payload[1]) == (len(payload) -2)
+                        isValidLenght := int(payload[1]) == len(payload[2:])
 			protocolNameStr := string(payload[4:])
 			//check protocol name
 			isValidMQTT := strings.HasPrefix(protocolNameStr, "MQ")
